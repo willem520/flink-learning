db24d302683611be60b82d782bc49eb84321eddd
willem <weiyu1990520@163.com>
2019-08-29 00:02:07 +0800

    调整项目

--- src/main/java/willem/weiyu/bigdata/flink/batch/JavaWordCount.java
+++ src/main/java/willem/weiyu/bigdata/flink/batch/JavaWordCount.java
@@ -2,12 +2,10 @@ package willem.weiyu.bigdata.flink.batch;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
-import willem.weiyu.bigdata.flink.stream.JavaStreamWordCount;
 
 import java.io.File;
 
